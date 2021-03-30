d8dc099c5400eda61e0fe0f416e91bded8bfadb1
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-30 14:55:43 +0500

    Step07:- Enable checkout option
    1:- Steps
    	a:- CUpdated
    		i:- src/pages/index.tsx to add corect price tag from stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Product } from "../components/Product";
 const productData = [
   {
     name: "Water Bottle",
-    sku: "price_1HiNkpEncg17e61HdiErZOkH",
+    sku: "price_1IFmnREKbjR9lGIBfLo3GnbE",
     price: 1500,
     image:
       "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/i1-6000a6ed-e59b-47ae-b305-64ebade945f0/710ml-tr-hypercharge-twist-water-bottle-Ft4Fgv.jpg",
@@ -13,7 +13,7 @@ const productData = [
   },
   {
     name: "Formal Shoe",
-    sku: "price_1HiJklEncg17e61HJvgC9VGR",
+    sku: "price_1IFmpxEKbjR9lGIBEk9T5OKD",
     price: 3000,
     image: "https://ebhfashion.com/wp-content/uploads/0341-1-054.jpg",
     currency: "USD",
