953041ac0077f3e4fc5a448ca38de86e9f1f56c6
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-30 14:59:20 +0500

    Step08:- create Payment succes page

---
+++ src/pages/payment-success.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import { useShoppingCart } from "use-shopping-cart";
+export default function Product({ product }) {
+  const { clearCart } = useShoppingCart();
+  useEffect(() => {
+    clearCart();
+  }, []);
+
+  return <div>Payment Successful</div>;
+}
